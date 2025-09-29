5c2bdd84c0d71af0ace48a5c5e508d1b863e2d2e
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-09-29 16:21:11 +0530

    refactor: avoid overspecialization of methods

---
+++ src/despecialize.jl
@@ -0,0 +1,43 @@
+struct __InternalInvalidator1 <: Real end
+struct __InternalInvalidator2 <: AbstractArray{Real, 1} end
+struct __InternalInvalidator3 <: AbstractArray{Number, 2} end
+symtype(::__InternalInvalidator1) = _unreachable()
+symtype(::__InternalInvalidator2) = _unreachable()
+symtype(::__InternalInvalidator3) = _unreachable()
+symtype(::Vector{Int}) = Vector{Int}
+symtype(::Matrix{Float64}) = Matrix{Float64}
+symtype(::Array{UInt, 3}) = Array{UInt, 3}
+symtype(::Int) = Int
+symtype(::Float64) = Float64
+symtype(::UInt) = UInt
+unwrap(::__InternalInvalidator1) = _unreachable()
+unwrap(::__InternalInvalidator2) = _unreachable()
+unwrap(::__InternalInvalidator3) = _unreachable()
+unwrap(x::AbstractArray{Int}) = x
+unwrap(x::AbstractArray{Float64}) = x
+unwrap(x::AbstractArray{UInt}) = x
+unwrap(x::Int) = x
+unwrap(x::Float64) = x
+unwrap(x::UInt) = x
+(::Rule)(::__InternalInvalidator1) = _unreachable()
+(::Rule)(::__InternalInvalidator2) = _unreachable()
+(::Rule)(::__InternalInvalidator3) = _unreachable()
+(::ACRule)(::__InternalInvalidator1) = _unreachable()
+(::ACRule)(::__InternalInvalidator2) = _unreachable()
+(::ACRule)(::__InternalInvalidator3) = _unreachable()
+promote_symtype(::__InternalInvalidator1, args...) = _unreachable()
+promote_symtype(::__InternalInvalidator2, args...) = _unreachable()
+promote_symtype(::__InternalInvalidator3, args...) = _unreachable()
+Base.convert(::Type{__InternalInvalidator1}, ::BasicSymbolic) = _unreachable()
+Base.convert(::Type{Int}, ::BasicSymbolic) = _unreachable()
+Base.convert(::Type{Float64}, ::BasicSymbolic) = _unreachable()
+Base.convert(::Type{UInt}, ::BasicSymbolic) = _unreachable()
+expand(x::Int, _...) = x
+expand(x::Complex{Int}, _...) = x
+expand(x::Complex{Float64}, _...) = x
+simplify(x::Int; _...) = x
+simplify(x::Complex{Int}; _...) = x
+simplify(x::Complex{Float64}; _...) = x
+isbinop(::Int) = false
+isbinop(::String) = false
+isbinop(::__InternalInvalidator1) = false
